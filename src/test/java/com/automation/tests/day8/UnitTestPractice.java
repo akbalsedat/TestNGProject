93cfaa75bdc1159e19997e8570e0b365cd03774d
akbalsedat <akbalsedat@gmail.com>
2020-03-14 11:45:54 -0400

    first commit

---
+++ src/test/java/com/automation/tests/day8/UnitTestPractice.java
@@ -0,0 +1,29 @@
+package com.automation.tests.day8;
+
+public class UnitTestPractice {
+
+    public static void main(String[] args) {
+        //Assertion
+        verifyEquals("cba", reverseString("abc"));
+    }
+
+    public static String reverseString(String str){
+        String reversedString = "";
+        for (int index = str.length() -1; index >= 0 ; index--) {
+            reversedString += str.charAt(index);
+        }
+        return reversedString;
+    }
+
+    public static boolean verifyEquals(String expected, String actual) {
+        if (expected.equals(actual)) {
+            System.out.println("TEST PASSED");
+            return true;
+        } else {
+            System.out.println("Test failed!!!");
+            System.out.println("Expected: " + expected);
+            System.out.println("Actual: " + actual);
+            return false;
+        }
+    }
+}
